3bf8cfae062a83796ab4f789870e79bdb41d9771
Scott Vorthmann <scott@vorthmann.org>
2017-05-06 12:48:38 -0700

    Experiment with ColladaLoader, TrackballControl, and lighting
    Loading my own vZome export as a .dae file, using ColladaLoader.
    
    Added TrackballControl.
    
    Attached directional lights to the camera.

--- main.js
+++ main.js
@@ -13,13 +13,11 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-          width: 800, 
-          height: 600, 
-          frame:false, 
-          transparent: true
+          width: 1400, 
+          height: 1200
   });
 
-  mainWindow.setIgnoreMouseEvents(true)
+  //mainWindow.setIgnoreMouseEvents(true)
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
